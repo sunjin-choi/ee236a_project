42bdc493ccd76009324bbe161294da5a037f1f0d
Sunjin Choi <sunjin_choi@berkeley.edu>
2022-12-15 19:11:08 -0800

    repo cleanup

--- run/lle_experiment.py
+++ run/lle_experiment.py
@@ -20,6 +20,54 @@ import time
 pio.renderers.default = "notebook"
 pio.templates.default = "seaborn"
 
+from matplotlib import pyplot as plt
+
+
+default_font = {'fontname': 'serif', 'fontsize': 16}
+
+def draw_nice_canvas(num_x=1, num_y=1):
+    """
+    Draw a beautiful gridded canvas and return fig, ax
+    """
+    fig, ax = plt.subplots(num_y, num_x, figsize=(20, 10))
+
+    if isinstance(ax, np.ndarray):
+        for i in range(num_x):
+            for j in range(num_y):
+                ax[i*num_y + j].grid()
+    else:
+        ax.grid()
+    # # set font to academic paper best fit
+    # plt.rc('font', family='serif', size=16)
+
+    plt.rcParams["font.family"] = "serif"
+    plt.rcParams["font.size"] = 16
+
+    return fig, ax
+
+def draw_grid_spec():
+    """
+    Draw a beautiful gridded canvas and return fig, ax
+    """
+    fig = plt.figure(figsize=(20, 10))
+    gs = fig.add_gridspec(2, 3)
+    ax = []
+
+    ax.append(fig.add_subplot(gs[0, 0:2]))
+    ax.append(fig.add_subplot(gs[1, 0:2]))
+    ax.append(fig.add_subplot(gs[0, 2]))
+    ax.append(fig.add_subplot(gs[1, 2]))
+
+    for j in range(4):
+        ax[j].grid()
+
+    # plt.rc('font', family='serif', size=16)
+
+    plt.rcParams["font.family"] = "Helvetica"
+    plt.rcParams["font.size"] = 16
+
+    return fig, ax
+
 
 class ExperimentConfig(NamedTuple):
     R: float
@@ -57,7 +105,7 @@ class LLEExperiment:
                 Qi=1e6, 
                 Qc=1e6, 
                 γ=3.2, 
-                dispfile="./RW1000_H430.csv"
+                dispfile="./config/RW1000_H430.csv"
         )
 
         sim = dict(
@@ -81,7 +129,7 @@ class LLEExperiment:
                 Qi=1e6, 
                 Qc=1e6, 
                 γ=1.55,
-                dispfile="./TestDispersion.csv"
+                dispfile="./config/TestDispersion.csv"
         )
 
         sim = dict(
@@ -106,7 +154,7 @@ class LLEExperiment:
                 Qi=config.Qi,
                 Qc=config.Qc,
                 γ=3.2,
-                dispfile="./RW1000_H430.csv"
+                dispfile="./config/RW1000_H430.csv"
         )
 
         sim = dict(
@@ -167,11 +215,161 @@ class LLEExperiment:
             os.mkdir(path)
         self.solver.SaveResults(fname, path)
 
+    def add_axes_comb_power(self, fig, ax, label, cnum):
+        fig, ax = self.PlotCombPower(fig, ax, label, cnum, which='comb', xaxis='steps')
+        return fig, ax
 
-    def get_axes_comb_power(self):
-        fig = self.solver.PlotCombPower(do_matplotlib=True, which='comb', xaxis='detuning')
-        return fig
+    def add_axes_comb_spectra(self, ind, fig, ax, label, cnum):
+        fig, ax = self.PlotCombSpectra(ind, fig, ax, label, cnum)
+        return fig, ax
 
-    def get_axes_comb_spectra(self, ind):
-        fig = self.solver.PlotCombSpectra(ind=ind, do_matplotlib=True)
-        return fig
+    def add_axes_dispersion(self, fig, ax, label):
+        fig, ax = self.PlotDispersion(fig, ax, label)
+        return fig, ax
+
+    def get_conversion_efficiency_from_spectra(self, ind):
+        comb_spectra = np.abs(self.solver.sol.Ecav[:, ind]**2)
+        comb_spectra_sort = np.flip(np.sort(comb_spectra))
+
+        return np.sum(comb_spectra_sort[1:]) / self.main_params.Pin * 1e2
+
+
+
+    def infrared(self, cnum):
+        """Returns a color for a given infrared value.
+        The color palette is a list of colors that can be used to represent different data.
+
+        :param cnum: The index of the color to be returned, usually normalized to 1
+        :return: The color at the index of the color palette
+        """
+        return plt.cm.get_cmap('plasma')(cnum)
+
+    def PlotCombPower(self, fig, ax, label, cnum, which = 'all', xaxis = 'steps'):
+        tr = []
+        if xaxis.lower() == 'steps':
+            x = np.linspace(0,4999,5000)
+            # xlabel = 'LLE steps'
+            xlabel = 'Laser Scan'
+        elif xaxis.lower() == 'detuning':
+            x = 1e-9*self.solver._sol['detuning']/(2*np.pi)
+            xlabel = 'Detuning (GHz)'
+
+        if which.lower() == 'all':
+            ax.plot(x, self.solver.sol.Pcav)
+            ax.plot(x, self.solver.sol.Pwg)
+            ax.plot(x, self.solver.sol.Pcomb)
+
+        if which.lower() == 'comb':
+            ax.plot(x, self.solver.sol.Pcomb*1e3, label=label, color=self.infrared(cnum))
+        if which.lower() == 'waveguide':
+            ax.plot(x, self.solver.sol.Pwg*1e3, label=label, color=self.infrared(cnum))
+        if which.lower() == 'cavity':
+            ax.plot(x, self.solver.sol.Pcav*1e3, label=label, color=self.infrared(cnum))
+
+        ax.legend()
+        ax.set_xlabel(xlabel, **default_font)
+        ax.set_ylabel('Power (mW)', **default_font)
+
+        return fig, ax
+
+    def PlotCombSpectra(self, ind, fig, ax, label, cnum, style = '', xaxis = 'freq', where = 'waveguide', floor = -100):
+        
+        if xaxis.lower() == 'frequencies' or xaxis.lower() == 'freq':
+            x = self.solver.sol.freq*1e-12
+            xlabel = 'Frequency (THz)'
+        elif xaxis.lower() == 'modes':
+            x = np.arange(self.solver._sim['mu_sim'][0],self.solver._sim['mu_sim'][-1])  - self.solver._sim['ind_pmp'][0]
+            xlabel = 'Mode Numbers'
+
+        if where == 'waveguide' or where =='wg':
+            y = 10*np.log10(np.abs(self.solver.sol.Ewg[:,ind])**2)+30
+            name = ['Waveguide']
+        if where == 'cavity' or where =='cav':
+            y = 10*np.log10(np.abs(self.solver.sol.Ecav[:,ind])**2)+30
+            name = ['Cavity']
+        if where == 'both':
+            y = 10*np.log10(np.abs(self.solver.sol.Ecav[:,ind])**2)+30
+            y2 = 10*np.log10(np.abs(self.solver.sol.Ewg[:,ind])**2)+30
+            name = ['Cavity', 'Waveguide']
+
+        if style == 'comb':
+            x_ = np.zeros(x.size*3)
+            x_[::3] = x
+            x_[1::3] = x
+            x_[2::3] = x
+            x = x_
+
+            y_ = np.zeros(y.size*3)
+            y_[::3] = floor
+            y_[1::3] = y
+            y_[2::3] = floor
+            y = y_
+
+            if where == 'both':
+                y2_ = np.zeros(y2.size*3)
+                y2_[::3] = floor
+                y2_[1::3] = y2
+                y2_[2::3] = floor
+                y2 = y2_
+
+
+        ax.plot(x, y, label = label, color = self.infrared(cnum))
+        if where == 'both':
+            ax.plot(x, y2, label = name[1])
+
+        # # draw vertical line at pump frequency
+        # ax.axvline(x=self.sim.f_pmp[0]*1e-12, color='k', linestyle='--', linewidth=1)
+        
+        ax.legend()
+        ax.set_xlabel(xlabel, **default_font)
+        ax.set_ylabel('Power (dBm)', **default_font)
+
+        self.solver._plotSpecta = True
+        self.solver._indSpectra = ind
+
+        return fig, ax
+
+
+
+    def PlotDispersion(self, fig, ax, label):
+        for ii in range(len(self.solver._analyze.fpmp)-1):
+            μfit = self.solver._analyze.μfit[ii] #+ self.solver._analyze.ind_pmp_fit[ii]
+            μsim = self.solver._analyze.μsim[ii] #+ self.solver._analyze.ind_pmp_sim[ii]
+            dν_fit = np.arange(μfit[0], μfit[-1]+1)*self.solver._analyze.D1[0]/(2*np.pi)
+            dν_sim = np.arange(μsim[0], μsim[-1]+1)*self.solver._analyze.D1[0]/(2*np.pi)
+            ν0 = self.solver._analyze.fpmp[ii]
+            rf = self.solver._analyze.rf
+            rf_fit = (ν0+dν_fit)
+            rf_sim = (ν0+dν_sim)
+
+            ax.plot(rf*1e-12,
+                   self.solver._analyze.Dint[ii]*1e-9/(2*np.pi),
+                   'o',ms= 3,
+                   label = 'FEM Simulation')
+
+
+            ax.plot(rf_fit*1e-12,
+                   self.solver._analyze.Dint_fit[ii]*1e-9/(2*np.pi),
+                   '--',
+                   label = 'Fit')
+            ax.plot(rf_sim*1e-12,
+                   self.solver._analyze.Dint_sim[ii]*1e-9/(2*np.pi),
+                   label = 'LLE simulation')
+
+            if self.solver._analyze.plottype.lower() == 'sim':
+                ax.plot(rf_sim*1e-12,
+                       self.solver._analyze.Dint_sim[ii]*1e-9/(2*np.pi),
+                       label = 'LLE simulation')
+
+            if self.solver._analyze.plottype.lower() == 'fit':
+                ax.plot(rf_fit*1e-12,
+                       self.solver._analyze.Dint_fit[ii]*1e-9/(2*np.pi),
+                       label = 'Fit')
+
+        # draw vertical line at pump frequency
+        ax.axvline(x=self.sim.f_pmp[0]*1e-12, color='k', linestyle='--', linewidth=1)
+
+        ax.legend()
+        ax.set_xlim([200, 400])
+        ax.set_ylim([-50, 50])
+        return fig, ax
--- run/main.py
+++ run/main.py
@@ -1,29 +1,26 @@
 
 
-from lle_experiment import LLEExperiment, ExperimentConfig
+from lle_experiment import LLEExperiment, ExperimentConfig, draw_nice_canvas, draw_grid_spec, default_font
 
-def test_save():
-    experiment = LLEExperiment.default_initializer()
-    experiment.run()
-    experiment.save_checkpoint("test")
-    return experiment
+from matplotlib import pyplot as plt
 
-def test_load():
-    experiment = LLEExperiment.load_checkpoint("test")
-    return experiment
-
-
-def test_sim():
-    config = ExperimentConfig(R=23e-6, Qi=1e6, Qc=1e6, Pin=160e-3)
-    experiment = LLEExperiment.initialize_from_config(config)
-    experiment.run()
-    experiment.save_checkpoint()
-    return experiment
-
-def test_load_sim():
-    config = ExperimentConfig(R=23e-6, Qi=1e6, Qc=1e6, Pin=160e-3)
-    experiment = LLEExperiment.load_checkpoint(config)
-    return experiment
+#def test_save():
+#    experiment = LLEExperiment.test_initialize()
+#    experiment.run()
+#    experiment.save_checkpoint()
+#    return experiment
+#
+#def test_sim():
+#    config = ExperimentConfig(R=23e-6, Qi=1e6, Qc=1e6, Pin=160e-3)
+#    experiment = LLEExperiment.initialize_from_config(config)
+#    experiment.run()
+#    experiment.save_checkpoint()
+#    return experiment
+#
+#def test_load_sim():
+#    config = ExperimentConfig(R=23e-6, Qi=1e6, Qc=1e6, Pin=160e-3)
+#    experiment = LLEExperiment.load_checkpoint(config)
+#    return experiment
 
 
 def sweep_Qc():
@@ -68,17 +65,147 @@ def load_sweep_Pin():
     return experiment_list
 
 
+
+
+def sweep_Qc_overcouple():
+    Qc_list = [1e5, 2e5, 3e5, 4e5, 5e5, 6e5, 7e5, 8e5, 9e5, 10e5]
+    experiment_list = []
+    for Qc in Qc_list:
+        config = ExperimentConfig(R=23e-6, Qi=1e6, Qc=Qc, Pin=500e-3)
+        experiment = LLEExperiment.initialize_from_config(config)
+        experiment.run()
+        experiment.save_checkpoint()
+        experiment_list.append(experiment)
+    return experiment_list
+
+def load_sweep_Qc_overcouple():
+    Qc_list = [1e5, 2e5, 3e5, 4e5, 5e5, 6e5, 7e5, 8e5, 9e5, 10e5]
+    experiment_list = []
+    for Qc in Qc_list:
+        config = ExperimentConfig(R=23e-6, Qi=1e6, Qc=Qc, Pin=500e-3)
+        experiment = LLEExperiment.load_checkpoint(config)
+        experiment_list.append(experiment)
+    return experiment_list
+
+
+def run():
+    sweep_Qc()
+    sweep_Pin()
+
+def run_overcouple():
+    sweep_Qc_overcouple()
+
+
+def plot():
+    experiment_Pin = load_sweep_Pin()
+    experiment_Qc = load_sweep_Qc()
+
+    fig, ax = draw_nice_canvas()
+    fig = experiment_Pin[0].solver.Analyze(plot=True)
+    fig.axes[0].set_xlim([100, 400])
+    fig.axes[0].set_ylim([-200, 100])
+
+    spectra_idx = 3600
+
+    fig, ax = draw_nice_canvas()
+    label = f"{experiment_Pin[3].main_params.Pin*1e3:.0f} mW"
+    fig, ax = experiment_Pin[3].add_axes_comb_power(fig, ax, label, 3/5)
+
+    ax.annotate(xy=(3000, 4.9), text="Single Soliton Step", arrowprops=dict(arrowstyle="->"), xytext=(3000, 10.9))
+    ax.annotate(xy=(1931, 16), text="Modulation Instability", arrowprops=dict(arrowstyle="->"), xytext=(1550, 10.9))
+    ax.annotate(xy=(971, 6), text="Primary Comb", arrowprops=dict(arrowstyle="->"), xytext=(400, 10.9))
+    
+    fig.savefig("./figure/dynamics.png", dpi=300, bbox_inches="tight")
+    # fig.show()
+    
+
+    eff_Pin = []
+    for i in range(len(experiment_Pin)):
+        eff_Pin.append(experiment_Pin[i].get_conversion_efficiency_from_spectra(spectra_idx))
+
+    eff_Qc = []
+    for i in range(len(experiment_Qc)):
+        eff_Qc.append(experiment_Qc[i].get_conversion_efficiency_from_spectra(spectra_idx))
+
+    fig, ax = draw_nice_canvas(2, 1)
+    ax[0].plot([experiment_Pin[i].main_params.Pin*1e3 for i in range(len(experiment_Pin))], eff_Pin, "o-")
+    ax[0].set_xlabel("Input power (mW)", **default_font)
+    ax[0].set_ylabel("Conversion Efficiency [%]", **default_font)
+    ax[0].set_title("Conversion Efficiency vs. Input Power", **default_font)
+
+    ax[1].plot([experiment_Qc[i].main_params.Qc*1e-6 for i in range(len(experiment_Qc))], eff_Qc, "o-")
+    ax[1].set_xlabel("Qc [x1e6]", **default_font)
+    ax[1].set_ylabel("Conversion Efficiency [%]", **default_font)
+    ax[1].set_title("Conversion Efficiency vs. Qc (Qi=1e6)", **default_font)
+    fig.savefig("./figure/conversion_efficiency.png", dpi=300, bbox_inches="tight")
+    # fig.show()
+
+
+    fig, ax = draw_grid_spec()
+    for i in range(len(experiment_Pin)):
+        label = f"{experiment_Pin[i].main_params.Pin*1e3:.0f} mW"
+        fig, ax[0] = experiment_Pin[i].add_axes_comb_power(fig, ax[0], label, i/len(experiment_Pin))
+
+    for i in range(len(experiment_Qc)):
+        label = f"{experiment_Qc[i].main_params.Qc*1e-6:.1f}e6 (Qi=1e6)"
+        fig, ax[1] = experiment_Qc[i].add_axes_comb_power(fig, ax[1], label, i/len(experiment_Qc))
+
+    for i in range(len(experiment_Pin)):
+        label = f"{experiment_Pin[i].main_params.Pin*1e3:.0f} mW"
+        fig, ax[2] = experiment_Pin[i].add_axes_comb_spectra(spectra_idx, fig, ax[2], label, i/len(experiment_Pin))
+
+    for i in range(len(experiment_Qc)):
+        label = f"{experiment_Qc[i].main_params.Qc*1e-6:.1f}e6 (Qi=1e6)"
+        fig, ax[3] = experiment_Qc[i].add_axes_comb_spectra(spectra_idx, fig, ax[3], label, i/len(experiment_Qc))
+
+    ax[2].yaxis.set_label_position("right")
+    ax[2].yaxis.tick_right()
+    ax[3].yaxis.set_label_position("right")
+    ax[3].yaxis.tick_right()
+
+    fig.savefig("./figure/dynamics_and_spectra.png", dpi=300, bbox_inches="tight")
+    # fig.show()
+
+
+    fig, ax = draw_nice_canvas(1, 1)
+    fig, ax = experiment_Pin[0].add_axes_dispersion(fig, ax, "Dispersion")
+    ax.set_xlim([200, 400])
+    ax.set_ylim([-50, 50])
+    ax.set_xlabel("Frequency (THz)")
+    ax.set_ylabel("Dispersion Dint (GHz)")
+    ax.set_title("Dispersion vs. Frequency")
+    fig.savefig("./figure/example_dispersion.png", dpi=300, bbox_inches="tight")
+    # fig.show()
+
+
+def plot_overcouple():
+
+    experiment_Qc = load_sweep_Qc_overcouple()
+    spectra_idx = 3600
+
+    fig, ax = draw_nice_canvas(2, 1)
+    for i in range(len(experiment_Qc)):
+        label = f"{experiment_Qc[i].main_params.Qc*1e-5:.1f}e5 (Qi=1e6)"
+        fig, ax[0] = experiment_Qc[i].add_axes_comb_power(fig, ax[0], label, i/len(experiment_Qc))
+
+    ax[0].set_title("Comb formation vs. Qc (Qi=1e6, Pin=500mW)", **default_font)
+
+    eff_Qc = []
+    for i in range(len(experiment_Qc)):
+        eff_Qc.append(experiment_Qc[i].get_conversion_efficiency_from_spectra(spectra_idx))
+    
+    ax[1].plot([experiment_Qc[i].main_params.Qc*1e-6 for i in range(len(experiment_Qc))], eff_Qc, "o-")
+    ax[1].set_xlabel("Qc [x1e6]", **default_font)
+    ax[1].set_ylabel("Conversion Efficiency [%]", **default_font)
+    ax[1].set_title("Conversion Efficiency vs. Qc (Qi=1e6, Pin=500mW)", **default_font)
+    fig.show()
+
+    fig.savefig("./figure/overcouple.png", bbox_inches="tight")
+
+
 if __name__ == "__main__":
-    experiment = test_sim()
-    # experiment = test_load_sim()
+    # run()
+    # run_overcouple()
+    plot()
+    plot_overcouple()
 
-    # experiment_Qc = sweep_Qc()
-    # experiment_Pin = sweep_Pin()
-
-    # experiment_Pin = load_sweep_Pin()
-    # experiment_Qc = load_sweep_Qc()
-
-    fig = experiment.solver.Analyze(plot=True, plottype="all")
-    # fig.axes[0].set_xlim([100, 400])
-    # fig.axes[0].set_ylim([-200, 100])
-    import pdb; pdb.set_trace()
